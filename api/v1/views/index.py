4ddcf0739e04f7a0f1f526b34b208a986642213b
TshidisoMp3 <tshidisomorake18@gmail.com>
2024-06-12 01:37:26 +0200

    Air Bnb Clone v3

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -10,3 +10,17 @@ from models import storage
 def status():
     """ the results of the status of the API"""
     return jsonify({"status": "OK"})
+
+
+@app_views.route('/stats', strict_slashes=False)
+def stats():
+    """ the results of the stats of the API"""
+    stats = {
+        "amenities": storage.count("Amenity"),
+        "cities": storage.count("City"),
+        "places": storage.count("Place"),
+        "reviews": storage.count("Review"),
+        "states": storage.count("State"),
+        "users": storage.count("User")
+    }
+    return jsonify(stats)
